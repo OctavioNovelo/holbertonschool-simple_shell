0b7af6d10f0ff80bd036717f8245ba5578879bfd
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-17 22:15:51 +0000

    Prompt

---
+++ prompt.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *prompt - print the prompt
+ *
+ */
+void prompt()
+{
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t str;
+  while (1)
+    {
+      printf("root@prueba# ");
+      str = getline(&line, &len, stdin);
+
+      if (str == -1)
+	{
+	  printf("Error reading input\n");
+	  break;
+	}
+
+      if (line[str - 1] == '\n')
+      {
+	line[str - 1] = '\0';
+      }
+
+      if (strcmp(line, "EXIT") == 0)
+	{
+	  break;
+	}
+      /** Aqui hay que poner la logica de las funciones **/
+    }
+printf("You entered: %s\n", line);
+free(line);
+}
