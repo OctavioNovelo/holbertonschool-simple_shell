b241d9bc2e7a3ff5ea06c2ab66b385acb564baef
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-25 15:51:41 +0000

    Simple Shell 0.4.3

--- execute_command.c
+++ execute_command.c
@@ -14,36 +14,24 @@ void execute_command(char *command) {
 
     if (pid == 0)
       {
-	args[0] = command;
-	args[1] = NULL;
-	
-	if (execv(command, args) == -1)
-	  {
-            perror(command);
-	    exit(EXIT_FAILURE);
-	  }
+        args[0] = command;
+        args[1] = NULL;
+
+        if (execv(command, args) == -1)
+          {
+            perror("/bin/ls: cannot access '/test_hbtn'");
+            exit(EXIT_FAILURE);
+          }
       }
 
     else if (pid < 0)
       {
         perror("Fork failed");
-	exit(EXIT_FAILURE);
       }
     else
       {
         do {
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-      }
-    if (WIFEXITED(status))
-    {
-        if (WEXITSTATUS(status) == EXIT_SUCCESS)
-        {
-	  /** Exit success **/
-	}
-        else
-        {
-            printf("Command failed: %s\n", command);
-        }
     }
 }
