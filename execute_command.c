804dd9c85dfeb1af4623c8e6aac8c20f03877a10
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-25 18:35:45 +0000

    Simple Shell 0.4.7

--- execute_command.c
+++ execute_command.c
@@ -5,8 +5,7 @@
  * @command: A string
  */
 
-void execute_command(char *command)
-{
+void execute_command(char *command) {
     pid_t pid;
     int status;
     char *args[2];
@@ -28,29 +27,11 @@ void execute_command(char *command)
     else if (pid < 0)
       {
         perror("Fork failed");
-	exit(EXIT_FAILURE);
       }
     else
       {
-        waitpid(pid, &status, 0);
-      }
-    
-    if (WIFEXITED(status))
-      { 
-	if (WIFEXITED(status) == EXIT_SUCCESS)
-	  {
-	    /** Comando exitoso **/
-	  }
-	else
-	  {
-	    /** Comando fallido **/
-	    printf("%s\n", command);
-	  }
-      }
-    
-    else
-      {
-	printf("%s\n", command);
-	exit(EXIT_FAILURE);
-      }
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 }
