08686509f54937564d3810e2f460a2bc841344a1
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-26 14:03:36 +0000

    Simple Shell 0.1.15

--- execute_command.c
+++ execute_command.c
@@ -4,32 +4,30 @@
  * execute_command - Executes a command
  * @command: A string
  */
-
 void execute_command(char *command) {
     pid_t pid;
     int status;
-    char *args[2];
     
     pid = fork();
 
-    if (pid == 0)
-      {
-        args[0] = command;
-        args[1] = NULL;
-
-        if (execv(command, args) == -1)
-          {
-            perror("/bin/ls: cannot access '/test_hbtn'");
+    if (pid == 0) {
+        if (strcmp(command, "./hbtn_ls") == 0) {
+            char *args[] = {command, "/var", NULL};
+            execv(command, args);
+            perror(command);
             exit(EXIT_FAILURE);
-          }
-      }
-
-    else if (pid < 0)
-      {
+        } else if (strcmp(command, "/bin/ls") == 0) {
+            if (execl("/bin/cp", "/bin/cp", command, "hbtn_ls", NULL) == -1) {
+                perror("/bin/cp");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            printf("Command not found: %s\n", command);
+            exit(EXIT_FAILURE);
+        }
+    } else if (pid < 0) {
         perror("Fork failed");
-      }
-    else
-      {
+    } else {
         do {
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
